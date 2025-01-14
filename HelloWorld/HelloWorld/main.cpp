c62478265822a7380673d2b55e7887b9c6849886
erictarrbelmont <erictarrbelmont@gmail.com>
2025-01-14 12:23:01 -0600

    Project initialization

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Eric Tarr on 1/9/25.
+//
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "AET 4220\n";
+    return 0;
+}
